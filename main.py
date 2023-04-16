75df15ab5e4d6b0ec6027b1cd4555ee9d5024e9f
lopyad <97178054+lopyad@users.noreply.github.com>
2023-04-16 16:05:27 +0000

    해당 분류의 모든 페이지의 책 정보를 추출하도록 변경
    max_page를 알아낸다음
    goPages를 이용해서
    모든 페이지를 추출

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from exporter import save_to_file
 from scrapper import extract_book_list
 
-book_list = extract_book_list("00")
-save_to_file(book_list, "00")
+idx = "00"
+print(f"{idx} 분류에 해당하는 책 내용 추출 시작")
+book_list = extract_book_list(idx)
+save_to_file(book_list, idx)
--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,18 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 #from selenium.webdriver.common.keys import Keys
 import time
+import re
+
+def extract_max_page_number(text):
+  # 전체 건수 추출
+  #total_count = re.search(r'전체 (\d+)건', text).group(1)
+  
+  # 최대 페이지 수 추출
+  max_page = re.search(r'(\d+)/(\d+) 페이지', text).group(2)
+  
+  #print("전체 건수:", total_count)
+  print("최대 페이지 수:", max_page)
+  return int(max_page)
 
 def extract_book_list(idx="00"):
   #setting
@@ -22,53 +34,59 @@ def extract_book_list(idx="00"):
   js_code = f"goSearchKdcOnly('{idx}', '', '');"
   driver.execute_script(js_code)
   driver.implicitly_wait(5)
-  
-  #해당 페이지, 해당 분류 전부 스크랩핑 시작
+
   book_data = []
   
-  # 책에 대한 정보가 담겨있는 <ul>태그 전부 가져오기
-  book_list  = driver.find_element(By.CLASS_NAME, "school_lib")
-  driver.implicitly_wait(3)
-  ul_tags = book_list.find_elements(By.TAG_NAME, "ul")
-  
-  #각각의 <ul>태그 자식인 <li>태그에서 정보 추출하기
-  for ul_tag in ul_tags:
-    #책 제목, 저자, 출판정보, 청구기호, 소장처, 책 이미지 링크, 대출 가능 여부
-    #title, author, publisher, call_number, location, image, rental
-    book = {
-      "title" : "None",
-      "author" : "None",
-      "publisher" : "None",
-      "call_number" : "None",
-      "location" : "None",
-      "image" : "None",
-      "rental" : "None"
-    }
+  #해당 분류의 페이지 수를 추출
+  max_page = extract_max_page_number(driver.find_element(By.CLASS_NAME, "bd_list_guide").text)
 
-    #추출해서 book에 넣기
-    book["title"] = ul_tag.find_element(By.CLASS_NAME, "bd_list_title").text
-    book["author"] = ul_tag.find_element(By.CLASS_NAME, "bd_list_writer").find_element(By.CLASS_NAME, "dd").text
-    book["publisher"] = ul_tag.find_element(By.CLASS_NAME, "bd_list_company").find_element(By.CLASS_NAME, "dd").text
-    book["call_number"] = ul_tag.find_element(By.CLASS_NAME, "bd_list_year").find_element(By.CLASS_NAME, "dd").text
-    book["location"] = ul_tag.find_element(By.CLASS_NAME, "bd_list_location").find_element(By.CLASS_NAME, "dd").text
-    book["rental"] = ul_tag.find_element(By.CLASS_NAME, "rental_box").text
-    book["image"] = ul_tag.find_element(By.TAG_NAME, "img").get_attribute("src")
-    
-    # # "book_image" 클래스에서 이미지 링크 가져오기
-    # book_img = ul_tag.find_element(By.TAG_NAME, "img").get_attribute("src")
-  
-    # # "bd_list_title" 클래스에서 클래스가 "bold"인 span의 텍스트 가져오기
-    # book_title = ul_tag.find_element(By.CLASS_NAME, "bd_list_title").text
+  #페이지 수만 큼 스크래핑 
+  for page in range(1, max_page+1):
+    #현재 페이지, 해당 분류 전부 스크래핑 시작
+    # 책에 대한 정보가 담겨있는 <ul>태그 전부 가져오기
+    print(f"{page}페이지 추출 시작")
+    # if page>=2:
+    #   print("refreshed")
+    #   driver.refresh
+    #   driver.implicitly_wait(3)
 
-    # book_writer = ul_tag.find_element(By.CLASS_NAME, "bd_list_writer").find_element(By.CLASS_NAME, "dd").text
-    # print(book_writer)
+    book_list  = driver.find_element(By.CLASS_NAME, "school_lib")
+    driver.implicitly_wait(3)
+    ul_tags = book_list.find_elements(By.TAG_NAME, "ul")
     
-    # # 가져온 내용을 dictionary에 저장
-    # book = {"title": book_title, "image": book_img}
-    book_data.append(book)
+    #각각의 <ul>태그 자식인 <li>태그에서 정보 추출하기
+    for ul_tag in ul_tags:
+      #책 제목, 저자, 출판정보, 청구기호, 소장처, 책 이미지 링크, 대출 가능 여부
+      #title, author, publisher, call_number, location, image, rental
+      book = {
+        "title" : "None",
+        "author" : "None",
+        "publisher" : "None",
+        "call_number" : "None",
+        "location" : "None",
+        "image" : "None",
+        "rental" : "None"
+      }
   
+      #추출해서 book에 넣기
+      book["title"] = ul_tag.find_element(By.CLASS_NAME, "bd_list_title").text
+      book["author"] = ul_tag.find_element(By.CLASS_NAME, "bd_list_writer").find_element(By.CLASS_NAME, "dd").text
+      book["publisher"] = ul_tag.find_element(By.CLASS_NAME, "bd_list_company").find_element(By.CLASS_NAME, "dd").text
+      book["call_number"] = ul_tag.find_element(By.CLASS_NAME, "bd_list_year").find_element(By.CLASS_NAME, "dd").text
+      book["location"] = ul_tag.find_element(By.CLASS_NAME, "bd_list_location").find_element(By.CLASS_NAME, "dd").text
+      book["rental"] = ul_tag.find_element(By.CLASS_NAME, "rental_box").text
+      book["image"] = ul_tag.find_element(By.TAG_NAME, "img").get_attribute("src")
+      
+      book_data.append(book)
+
+    #다음 페이지로 이동하기
+    print(f"{page}페이지 추출 완료")
+    js_code = f"goPagesOnly({page+1})"
+    driver.execute_script(js_code)
+    driver.implicitly_wait(5)
+    
   # 결과 출력
-  print(book_data)
+  #print(book_data.count)
   
   # 웹드라이버 종료
   time.sleep(3)
